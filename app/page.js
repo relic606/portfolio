efe4e9faf74c6529c8d6f52998a91cfefffe7b3c
Andy Anderson <relic606@yahoo.com>
2023-07-19 14:55:30 -0600

    home page setup

--- app/page.js
+++ app/page.js
@@ -1,6 +1,27 @@
-import resetStyles from "../styles/reset.css";
+// import resetStyles from "../styles/reset.css";
 import globalStyles from "../styles/global.css";
+import Image from "next/image";
 
 export default function Home() {
-	return <main>Hello World man dude</main>;
+	return (
+		<main className="flex flex-col">
+			<div className="flex place-content-around">
+				<h2>Andrew Anderson</h2>
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>Projects</li>
+						<li>Skills</li>
+					</ul>
+				</nav>
+			</div>
+
+			<Image
+				alt="My_Photo"
+				height={300}
+				width={300}
+				className="place-self-center"
+			></Image>
+		</main>
+	);
 }
