0f68456751238902d82dacfce3dd53dc33954d9a
Andy Anderson <relic606@yahoo.com>
2023-08-04 10:14:17 -0600

    minor changes to project list

--- app/components/ProjectList.js
+++ app/components/ProjectList.js
@@ -1,86 +1,88 @@
 import Image from "next/image";
 
 const PROJECTS = [
-  {
-    title: "Card Game",
-    description: "Card game built with React.js",
-    image: "/react-logo.png",
-    link: "/projects/game",
-    linkText: "https://andrewtanderson.com",
-  },
-  {
-    title: "DevMountain Web Dev Capstone",
-    description: "Full-stack event planner app",
-    image: "/full-stack.png",
-    link: "https://share.vidyard.com/watch/gWtgwMGcd6L36fTpUE8NQS?",
-    linkText: "https://share.vidyard.com",
-  },
-  {
-    title: "DevMountain Foundations Capstone",
-    description: "Full-stack workout planner app",
-    image: "/full-stack.png",
-    link: "https://share.vidyard.com/watch/QdoAUHpTjcZ34SjP5bZSqT?",
-    linkText: "https://share.vidyard.com",
-  },
-  {
-    title: "Terran Emporium",
-    description: "Codepen HTML, CSS, and Javascript Project",
-    image: "/codepen-logo.png",
-    link: "https://codepen.io/relic606/full/LYOyvYg",
-    linkText: "https://codepen.io",
-  },
+	{
+		title: "Card Game",
+		description: "Card game built with React.js",
+		image: "/game-logo.png",
+		link: "/projects/game",
+		linkText: "https://andrewtanderson.com"
+	},
+	{
+		title: "DevMountain Web Dev Capstone",
+		description: "Full-stack event planner app",
+		image: "/full-stack.png",
+		link: "https://share.vidyard.com/watch/gWtgwMGcd6L36fTpUE8NQS?",
+		linkText: "https://share.vidyard.com"
+	},
+	{
+		title: "DevMountain Foundations Capstone",
+		description: "Full-stack workout planner app",
+		image: "/full-stack.png",
+		link: "https://share.vidyard.com/watch/QdoAUHpTjcZ34SjP5bZSqT?",
+		linkText: "https://share.vidyard.com"
+	},
+	{
+		title: "Terran Emporium",
+		description: "Codepen HTML, CSS, and Javascript Project",
+		image: "/codepen-logo.png",
+		link: "https://codepen.io/relic606/full/LYOyvYg",
+		linkText: "https://codepen.io"
+	}
 ];
 
 export default function ProjectList() {
-  return (
-    <section className="max-w-7xl p-12 my-6">
-      <div className="ml-24 mb-4">
-        <h1 className="text-3xl font-semibold">Title text</h1>
-        <p className="pt-4 pl-1">additional text</p>
-      </div>
-      <div className="flex flex-wrap pl-20">
-        {PROJECTS.map((project) => {
-          return (
-            <div
-              key={project.title}
-              className="mx-4 h-60 w-80 flex flex-col justify-around py-4"
-            >
-              <Image
-                src={project.image}
-                alt={project.title + " image"}
-                width={50}
-                height={50}
-                className="border rounded-full p-2 border-gray-100 shadow-sm shadow-gray-400"
-              />
-              <h2 className="font-semibold">{project.title}</h2>
-              <p className="font-normal text-sm">{project.description}</p>
-              <a
-                target="_blank"
-                href={project.link}
-                className="opacity-40 text-sm flex"
-              >
-                <div className="px-1 flex justify-center pt-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-4 h-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                    />
-                  </svg>
-                </div>
-                <div>{project.linkText}</div>
-              </a>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+	return (
+		<section className="max-w-7xl p-12 my-6">
+			<div className="ml-24 mb-4">
+				<h1 className="text-3xl font-semibold">Title text</h1>
+				<p className="pt-4 pl-1">additional text</p>
+			</div>
+			<div className="flex flex-wrap pl-20">
+				{PROJECTS.map((project) => {
+					return (
+						<div
+							key={project.title}
+							className="mx-4 h-60 w-80 flex flex-col justify-around py-4"
+						>
+							<Image
+								src={project.image}
+								alt={project.title + " image"}
+								width={50}
+								height={50}
+								className="border rounded-full p-2 border-gray-100 shadow-sm shadow-gray-400"
+							/>
+							<h2 className="font-semibold">{project.title}</h2>
+							<p className="font-normal text-sm text-gray-600">
+								{project.description}
+							</p>
+							<a
+								target="_blank"
+								href={project.link}
+								className="opacity-40 text-sm flex"
+							>
+								<div className="px-1 flex justify-center pt-1">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-4 h-4"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+										/>
+									</svg>
+								</div>
+								<div>{project.linkText}</div>
+							</a>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,88 +1,88 @@
 import Image from "next/image";
 
 const SKILLS = [
-  {
-    language: "Javascript",
-    description: "Super good at it",
-    year: "2012",
-    image: "/javascript-logo.png",
-  },
-  {
-    language: "HTML & CSS",
-    description: "Just amazing, truly",
-    year: "2021",
-    image: "/html-css-logo.png",
-  },
-  {
-    language: "React.js",
-    description: "Pretty good at that too",
-    year: "2022",
-    image: "/react-logo-skills.png",
-  },
-  {
-    language: "Next.js",
-    description: "Awesome in every way",
-    year: "2022",
-    image: "/next-logo.png",
-  },
-  {
-    language: "Tailwind CSS",
-    description: "Wow, an inspiration",
-    year: "2022",
-    image: "/tailwind-logo.png",
-  },
-  {
-    language: "PostgreSQL",
-    description: "Saved stuff to database",
-    year: "2022",
-    image: "/postgresql-logo.png",
-  },
+	{
+		language: "Javascript",
+		description: "Super good at it",
+		year: "2012",
+		image: "/javascript-logo.png"
+	},
+	{
+		language: "HTML & CSS",
+		description: "Just amazing, truly",
+		year: "2021",
+		image: "/html-css-logo.png"
+	},
+	{
+		language: "React.js",
+		description: "Pretty good at that too",
+		year: "2022",
+		image: "/react-logo-skills.png"
+	},
+	{
+		language: "Next.js",
+		description: "Awesome in every way",
+		year: "2022",
+		image: "/next-logo.png"
+	},
+	{
+		language: "Tailwind CSS",
+		description: "Wow, an inspiration",
+		year: "2022",
+		image: "/tailwind-logo.png"
+	},
+	{
+		language: "PostgreSQL",
+		description: "Saved stuff to database",
+		year: "2022",
+		image: "/postgresql-logo.png"
+	}
 ];
 
 export default function Skills() {
-  return (
-    <section className="flex flex-col w-2/5 place-content-center px-8 border h-fit my-auto rounded-2xl">
-      <div className="flex items-center pl-1 my-4 pt-4">
-        <Image src="/skills.png" alt="skills image" width={30} height={30} />
-        <div className="pl-4 font-semibold">Skills</div>
-      </div>
-      <div className="flex flex-col">
-        {SKILLS.map((skill) => {
-          return (
-            <div key={skill.language} className="flex items-center py-4">
-              <Image
-                src={skill.image}
-                alt={skill.language + " logo"}
-                width={50}
-                height={50}
-                className="p-2 border border-gray-100 rounded-full shadow-sm shadow-gray-400"
-              ></Image>
-              <div className="flex flex-col w-full pl-4 ">
-                <p className="font-semibold">{skill.language}</p>
-                <div className="flex justify-between text-gray-500 font-normal">
-                  <div className="flex flex-wrap w-2/3 text-gray-500">
-                    {" "}
-                    {skill.description}
-                  </div>
-                  <div className="w-fit ml-4 text-gray-400">
-                    {skill.year} &mdash; Present{" "}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="w-full flex justify-center pt-4 pb-8 font-semibold">
-        Download Resume{" "}
-        <Image
-          src="/download.png"
-          alt="download image"
-          width={25}
-          height={25}
-          className="opacity-60 mt-1"
-        ></Image>
-      </div>
-    </section>
-  );
+	return (
+		<section className="flex flex-col w-2/5 place-content-center px-8 border h-fit my-auto rounded-2xl">
+			<div className="flex items-center my-4 pt-4">
+				<Image src="/skills.png" alt="skills image" width={30} height={30} />
+				<div className="pl-4 font-semibold">Skills</div>
+			</div>
+			<div className="flex flex-col">
+				{SKILLS.map((skill) => {
+					return (
+						<div key={skill.language} className="flex items-center py-4">
+							<Image
+								src={skill.image}
+								alt={skill.language + " logo"}
+								width={50}
+								height={50}
+								className="p-2 border border-gray-100 rounded-full shadow-sm shadow-gray-400"
+							></Image>
+							<div className="flex flex-col w-full pl-4 ">
+								<p className="font-semibold">{skill.language}</p>
+								<div className="flex justify-between text-gray-500 font-normal">
+									<div className="flex flex-wrap w-2/3 text-gray-500">
+										{" "}
+										{skill.description}
+									</div>
+									<div className="w-fit ml-4 text-gray-400">
+										{skill.year} &mdash; Present{" "}
+									</div>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+			<div className="w-full flex justify-center pt-4 pb-8 font-semibold">
+				Download Resume{" "}
+				<Image
+					src="/download.png"
+					alt="download image"
+					width={25}
+					height={25}
+					className="opacity-60 mt-1"
+				></Image>
+			</div>
+		</section>
+	);
 }
