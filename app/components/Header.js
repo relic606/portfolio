ef1491616873d330859cc83c1341d8b85a897099
Andy Anderson <relic606@yahoo.com>
2023-08-09 10:18:56 -0600

    adjusted about page css

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,43 +4,45 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
-  const pathName = usePathname();
-  console.log(pathName);
-  return (
-    <header className="flex place-content-around bg-sky-950 text-white py-2">
-      <Link href={"/"}>
-        <h2 className="flex justify-center my-auto text-xl hover:text-sky-500 transition delay-50">
-          Andrew Anderson
-        </h2>
-      </Link>
-      <nav>
-        <ul className="flex">
-          <li className="px-8">
-            <Link
-              href={"/"}
-              className={`rounded-full ${
-                pathName === "/"
-                  ? "text-black bg-white p-1"
-                  : "hover:text-sky-500 transition delay-50"
-              }`}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/projects"}
-              className={`rounded-full ${
-                pathName === "/projects"
-                  ? "text-black bg-white p-1"
-                  : "hover:text-sky-500 transition delay-50"
-              }`}
-            >
-              Projects
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const pathName = usePathname();
+	console.log(pathName);
+	return (
+		<header className="bg-sky-950 text-white py-2">
+			<div className="flex max-w-screen-2xl justify-between mx-auto px-2">
+				<Link href={"/"}>
+					<h2 className="flex justify-center my-auto text-xl hover:text-sky-500 transition delay-50">
+						Andrew Anderson
+					</h2>
+				</Link>
+				<nav>
+					<ul className="flex">
+						<li className="px-8">
+							<Link
+								href={"/"}
+								className={`rounded-full ${
+									pathName === "/"
+										? "text-black bg-white p-1"
+										: "hover:text-sky-500 transition delay-50"
+								}`}
+							>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link
+								href={"/projects"}
+								className={`rounded-full ${
+									pathName === "/projects"
+										? "text-black bg-white p-1"
+										: "hover:text-sky-500 transition delay-50"
+								}`}
+							>
+								Projects
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 }
--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,90 +1,92 @@
 import Image from "next/image";
 
 const SKILLS = [
-  {
-    language: "React.js",
-    description: "Proficient in component-driven UI",
-    year: "2022",
-    image: "/react-logo-skills.png",
-  },
-  {
-    language: "Next.js",
-    description: "Versatile with modern React toolkits",
-    year: "2022",
-    image: "/next-logo.png",
-  },
-  {
-    language: "Tailwind CSS",
-    description: "Mastering utility-first design",
-    year: "2022",
-    image: "/tailwind-logo.png",
-  },
-  {
-    language: "PostgreSQL",
-    description: "Skilled in relational databases",
-    year: "2022",
-    image: "/postgresql-logo.png",
-  },
-  {
-    language: "Javascript",
-    description: "Expert in dynamic scripting",
-    year: "2012",
-    image: "/javascript-logo.png",
-  },
-  {
-    language: "HTML & CSS",
-    description: "Crafting responsive web designs",
-    year: "2021",
-    image: "/html-css-logo.png",
-  },
+	{
+		language: "React.js",
+		description: "Proficient in component-driven UI",
+		year: "2022",
+		image: "/react-logo-skills.png"
+	},
+	{
+		language: "Next.js",
+		description: "Versatile with modern React toolkits",
+		year: "2022",
+		image: "/next-logo.png"
+	},
+	{
+		language: "Tailwind CSS",
+		description: "Mastering utility-first design",
+		year: "2022",
+		image: "/tailwind-logo.png"
+	},
+	{
+		language: "PostgreSQL",
+		description: "Skilled in relational databases",
+		year: "2022",
+		image: "/postgresql-logo.png"
+	},
+	{
+		language: "Javascript",
+		description: "Expert in dynamic scripting",
+		year: "2012",
+		image: "/javascript-logo.png"
+	},
+	{
+		language: "HTML & CSS",
+		description: "Crafting responsive web designs",
+		year: "2021",
+		image: "/html-css-logo.png"
+	}
 ];
 
 export default function Skills() {
-  return (
-    <section className="flex flex-col w-2/5 place-content-center px-8 border h-fit my-auto rounded-2xl">
-      <div className="flex items-center my-4 pt-4">
-        <Image src="/skills.png" alt="skills image" width={30} height={30} />
-        <div className="pl-4 font-semibold">Skills</div>
-      </div>
-      <div className="flex flex-col">
-        {SKILLS.map((skill) => {
-          return (
-            <div key={skill.language} className="flex items-center py-4">
-              <Image
-                src={skill.image}
-                alt={skill.language + " logo"}
-                width={50}
-                height={50}
-                className="p-2 border border-gray-100 rounded-full shadow-sm shadow-gray-400"
-              ></Image>
-              <div className="flex flex-col w-full pl-4 ">
-                <p className="font-semibold">{skill.language}</p>
-                <div className="flex justify-between text-gray-500 font-normal">
-                  <div className="flex flex-wrap w-2/3 text-gray-500">
-                    {" "}
-                    {skill.description}
-                  </div>
-                  <div className="w-fit ml-4 text-gray-400">
-                    {skill.year} &mdash; Present{" "}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="w-full flex justify-center pt-4 pb-8 font-semibold">
-        <a href="/resume.pdf" download className="flex">
-          Download Resume{" "}
-          <Image
-            src="/download.png"
-            alt="download image"
-            width={25}
-            height={25}
-            className="opacity-60 mt-1"
-          ></Image>
-        </a>
-      </div>
-    </section>
-  );
+	return (
+		<section className="flex flex-col w-2/5 place-content-center px-8 border h-fit my-auto rounded-2xl">
+			<div className="flex items-center my-4 pt-4">
+				<Image src="/skills.png" alt="skills image" width={30} height={30} />
+				<div className="pl-4 font-semibold">Skills</div>
+			</div>
+			<div className="flex flex-col">
+				{SKILLS.map((skill) => {
+					return (
+						<div key={skill.language} className="flex items-center py-4">
+							<Image
+								src={skill.image}
+								alt={skill.language + " logo"}
+								width={50}
+								height={50}
+								className="p-2 border border-gray-100 rounded-full shadow-sm shadow-gray-400"
+							></Image>
+							<div className="flex flex-col w-full pl-4 ">
+								<p className="font-semibold">{skill.language}</p>
+								<div className="flex justify-between text-gray-500 font-normal">
+									<div className="flex flex-wrap w-2/3 text-gray-500">
+										{" "}
+										{skill.description}
+									</div>
+									<div className="w-fit ml-4 text-gray-400">
+										{skill.year} &mdash; Present{" "}
+									</div>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+			<a
+				href="/resume.pdf"
+				download
+				className="w-full flex justify-center pt-4 pb-4 mb-4 font-semibold rounded-md bg-gray-50"
+			>
+				Download Resume{" "}
+				<Image
+					src="/download.png"
+					alt="download image"
+					width={25}
+					height={25}
+					className="opacity-60 mt-1"
+				></Image>
+			</a>
+		</section>
+	);
 }
