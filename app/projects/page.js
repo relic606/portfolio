8c791e9efd3c03a0e89e57a26e0a281043f760eb
Andy Anderson <relic606@yahoo.com>
2023-08-30 09:58:01 -0600

    projects page style changes

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 import ProjectList from "../components/ProjectList.js";
 
 export default function Projects() {
-  return (
-    <main>
-      <div className="w-fit bg-white flex mx-auto my-16 rounded-lg">
-        <ProjectList />
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="w-fit bg-white flex mx-auto my-16 rounded-lg">
+				<ProjectList />
+			</div>
+		</main>
+	);
 }
