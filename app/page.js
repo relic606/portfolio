a9efdb52b46ae5ebaa9ccbbc6b88122e4658bd9a
Andy Anderson <relic606@yahoo.com>
2023-10-19 15:50:29 -0600

    minor css change

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import ProfileCircle from "./components/ProfileCircle";
 
 export default function App() {
 	return (
-		<main className="flex flex-row place-content-center m-16 rounded-lg bg-white p-8 max-w-screen-2xl mx-auto flex-wrap">
+		<main className="flex flex-row place-content-center m-16 rounded-lg bg-white p-8 max-w-screen-2xl mx-auto flex-wrap mt-12">
 			<section className="rounded flex place-content-center flex-col p-6 about-section">
 				<div className="flex">
 					<ProfileCircle />
